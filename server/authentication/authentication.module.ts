9d80c37ce3556bc1ca21beff39535203ff272993
Stanislas Bernard <bernardstanislas@gmail.com>
2020-03-20 15:40:00 +0100

    :bug: Fix prod build

--- server/authentication/authentication.module.ts
+++ server/authentication/authentication.module.ts
@@ -1,9 +1,37 @@
-import { Module } from "@nestjs/common";
+import {
+  Module,
+  NestModule,
+  MiddlewareConsumer,
+  RequestMethod
+} from "@nestjs/common";
 import { LocalStragy } from "./local.strategy";
 import { AuthenticationController } from "./authentication.controller";
+import passport from "passport";
 
 @Module({
   controllers: [AuthenticationController],
   providers: [LocalStragy]
 })
-export class AuthenticationModule {}
+export class AuthenticationModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    // Passport
+    consumer
+      .apply(passport.authenticate("fcp.integ01.dev-franceconnect.fr", {}))
+      .forRoutes({ path: "login", method: RequestMethod.GET });
+    consumer
+      .apply(
+        passport.authenticate("fcp.integ01.dev-franceconnect.fr", {
+          callback: true,
+          successRedirect: "/demarches/inscription-en-creche/famille"
+        } as any)
+      )
+      .forRoutes("login-callback");
+    consumer
+      .apply(
+        passport.authenticate("local", {
+          successRedirect: "/demarches/inscription-en-creche/revenus"
+        })
+      )
+      .forRoutes({ path: "login", method: RequestMethod.POST });
+  }
+}
